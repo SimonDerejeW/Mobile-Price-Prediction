da89761cdc48a89129b76efc552e0d21d451120d
Simon <simondereje1@gmail.com>
2024-11-04 18:40:52 +0300

    added port

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 import joblib
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -26,4 +27,5 @@ def predict():
     return render_template('index.html', prediction_text=f'Predicted Mobile Price: {prediction[0]:.2f} Cartoon Dollars')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable or default to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
